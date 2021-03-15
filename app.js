be567cfcb58b48387d52a8c0eafa7bd07851520d
David Wu <david204code@gmail.com>
2021-03-15 23:54:39 +0000

    Add the eventListener to remove/add the active css class for the buttons

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const btn = document.querySelectorAll(".tab-btn");
-const about = document.querySelector("about");
+const about = document.querySelector(".about");
 const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+  // console.log(e.target);
+  // console.log(e.target.dataset.id);
+  const id = e.target.dataset.id;
+  if (id) {
+    // first remove the class of active from all the button "tab-btn active"
+    btn.forEach(function (btn) {
+      btn.classList.remove("active");
+
+      // add the class of active to the button clicked
+      e.target.classList.add("active");
+    });
+  }
+});
